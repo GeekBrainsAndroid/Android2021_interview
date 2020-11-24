e42fcf431356babcfb0127b8a36a5bef8d0bcf2d
DwarfPorter <mrvlmor@gmail.com>
2020-11-24 09:42:11 +0300

    OCP: Решение проблемы нарушения принципа открытости-закрытости

--- Lesson-1/SOLID/OCP/app/src/main/java/ru/geekbrains/ocp/MainActivity.kt
+++ Lesson-1/SOLID/OCP/app/src/main/java/ru/geekbrains/ocp/MainActivity.kt
@@ -2,42 +2,37 @@ package ru.geekbrains.ocp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.DecimalFormat
 import kotlin.math.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        buttonSqr.setOnClickListener(this)
-        buttonSqrRoot.setOnClickListener(this)
-        buttonCube.setOnClickListener(this)
-        buttonLn.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View?) {
-        val value = editNumber.toDouble()
-        var result: Double = 0.0
-        when(v!!.id){
-            R.id.buttonSqr -> {
-                result = value.pow(2)
-            }
-            R.id.buttonSqrRoot ->{
-                result = sqrt(value)
-            }
-            R.id.buttonCube ->{
-                result = value.pow(3)
-            }
-            R.id.buttonLn ->{
-                result = log(value, E)
-            }
+        buttonSqr.setOnClickListener {
+            val value = editNumber.toDouble()
+            val result = value.pow(2)
+            textResult.text = DecimalFormat("#0.00").format(result)
         }
-        textResult.text = DecimalFormat("#0.00").format(result)
+        buttonSqrRoot.setOnClickListener {
+            val value = editNumber.toDouble()
+            val result = sqrt(value)
+            textResult.text = DecimalFormat("#0.00").format(result)
+        }
+        buttonCube.setOnClickListener {
+            val value = editNumber.toDouble()
+            val result = value.pow(3)
+            textResult.text = DecimalFormat("#0.00").format(result)
+        }
+        buttonLn.setOnClickListener {
+            val value = editNumber.toDouble()
+            val result = log(value, E)
+            textResult.text = DecimalFormat("#0.00").format(result)
+        }
     }
 
-    private fun EditText.toDouble () = this.text.toString().toDouble()
+    private fun EditText.toDouble() = this.text.toString().toDouble()
 }
